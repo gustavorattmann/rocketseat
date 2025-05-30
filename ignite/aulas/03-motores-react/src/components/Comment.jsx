2ce8a4f79c28fc16ed9f14ea23f1439f2e2f9e7d
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2025-05-29 23:11:18 -0300

    Motores do React: Realizando likes nos comentários

--- ignite/aulas/03-motores-react/src/components/Comment.jsx
+++ ignite/aulas/03-motores-react/src/components/Comment.jsx
@@ -2,10 +2,17 @@ import { ThumbsUpIcon, TrashIcon } from "@phosphor-icons/react";
 import styles from "./Comment.module.css";
 
 import { Avatar } from "./Avatar";
+import { useState } from "react";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -27,9 +34,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUpIcon />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
