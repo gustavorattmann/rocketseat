0a39063f7add1ce8375338d06476b10f11b0dc0c
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2025-05-29 22:10:39 -0300

    Motores do React: Inserindo comentários

--- ignite/aulas/03-motores-react/src/components/Comment.jsx
+++ ignite/aulas/03-motores-react/src/components/Comment.jsx
@@ -3,7 +3,7 @@ import styles from "./Comment.module.css";
 
 import { Avatar } from "./Avatar";
 
-export function Comment() {
+export function Comment({ content }) {
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/gustavorattmann.png" />
@@ -20,7 +20,7 @@ export function Comment() {
               <TrashIcon size={24} />
             </button>
           </header>
-          <p>Muito bom Devon, parabéns!! 👏👏</p>
+          <p>{content}</p>
         </div>
         <footer>
           <button>
